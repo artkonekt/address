ff3ce5f07c664980a37e7b5d2fd58b44a2183b76
Attila Fulop <fulopattila122@gmail.com>
2016-11-30 14:22:09 +0200

    Added mandatory files to act as a concord module

---
+++ src/Providers/ModuleServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Contains the ModuleServiceProvider.php class.
+ *
+ * @copyright   Copyright (c) 2016 Attila Fulop
+ * @author      Attila Fulop
+ * @license     MIT
+ * @since       2016-11-30
+ *
+ */
+
+
+namespace Konekt\Location\Providers;
+
+use Konekt\Concord\AbstractModuleServiceProvider;
+
+class ModuleServiceProvider extends AbstractModuleServiceProvider
+{
+
+}
---
+++ src/resources/mainfest.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name'    => 'Konekt Location Module',
+    'version' => '0.1.0'
+];
