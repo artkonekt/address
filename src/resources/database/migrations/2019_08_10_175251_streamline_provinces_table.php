f8fb4c7421786393cb5f3a9e73cd71b0fd7707a1
Attila Fulop <fulopattila122@gmail.com>
2019-08-11 09:52:42 +0200

    The province.type field is no longer an enum type in DB; Model is intact

---
+++ src/resources/database/migrations/2019_08_10_175251_streamline_provinces_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Konekt\Address\Models\ProvinceTypeProxy;
+
+class StreamlineProvincesTable extends Migration
+{
+    /**
+     * With Laravel up to v5.8 renaming any column in a table that also has a column
+     * of type enum is not currently supported. This problem only seems to affect
+     * MySQL, this workaround provides a bit dirty but working solution for it
+     *
+     * @see https://laravel.com/docs/5.8/migrations#modifying-columns
+     * @see https://stackoverflow.com/q/33140860/1016746
+     */
+    public function __construct()
+    {
+        $platform = DB::getDoctrineSchemaManager()->getDatabasePlatform();
+
+        if (!$platform->hasDoctrineTypeMappingFor('enum')) {
+            $platform->registerDoctrineTypeMapping('enum', 'string');
+        }
+    }
+
+    public function up()
+    {
+        /** based on ISO 3166-2 https://en.wikipedia.org/wiki/ISO_3166-2 */
+        Schema::table('provinces', function (Blueprint $table) {
+            // Get rid of enum field, they're fkcn painful with Laravel
+            $table->string('type', 16)->default(ProvinceTypeProxy::defaultValue())->change();
+            $table->index('code', 'provinces_code_index');
+            $table->unique(['country_id', 'code'], 'provinces_country_id_code_index');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('provinces', function (Blueprint $table) {
+            $table->dropUnique('provinces_country_id_code_index');
+            $table->dropIndex('provinces_code_index');
+            $table->enum('type', ProvinceTypeProxy::values())->default(ProvinceTypeProxy::defaultValue())->change();
+        });
+    }
+}
