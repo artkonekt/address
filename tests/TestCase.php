59e3bd9ca3cd73a3e7b5589c39c2ebb433b1d683
Attila Fulop <fulopattila122@gmail.com>
2020-09-09 10:01:28 +0200

    Attempt 2 to fix recent failure with SQLite test cases

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -58,7 +58,7 @@ abstract class TestCase extends Orchestra
             'host'     => '127.0.0.1',
             'username' => env('TEST_DB_USERNAME', 'pgsql' === $engine ? 'postgres' : 'root'),
             'password' => env('TEST_DB_PASSWORD', ''),
-            'foreign_key_constraints' => true,
+            'foreign_key_constraints' => false,
         ]);
 
         if ('pgsql' === $engine) {
