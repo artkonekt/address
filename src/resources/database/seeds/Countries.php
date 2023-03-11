5bd1321863626b9fc914feba10c38704f5219f86
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2023-03-11 20:36:48 +0200

    Added that extra line for beauty 💅

--- src/resources/database/seeds/Countries.php
+++ src/resources/database/seeds/Countries.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * Contains the db seeder with all the countries of the world
  *
--- src/resources/database/seeds/ProvincesAndRegionsOfBelgium.php
+++ src/resources/database/seeds/ProvincesAndRegionsOfBelgium.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * Contains the db seeder with the provinces of the Belgium
  *
